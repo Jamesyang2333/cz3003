18002382a121a910e389f582ba5da41dc18d7403
guomk <gmk19990312@gmail.com>
2019-03-27 17:06:06 +0800

    Refine tab structure

--- frontend/src/components/layout/tabContainer.js
+++ frontend/src/components/layout/tabContainer.js
@@ -1,63 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 
 import LocationSearchInput from '../map/LocationSearchInput';
 import MarkerPoint from '../map/Map';
-import Weather from '../weather/weather';
 import TestList from './testList';
 
 import { connect } from 'react-redux';
 import { getAll } from '../../actions/crisisAction';
-
-const styles = {
-  row: {
-    display: 'flex',
-    flexDirection: 'row wrap',
-    width: '100%',
-    marginTop: 30
-  },
-  divLeft: {
-    flex: 1,
-    height: '100%',
-    margin: 30,
-    marginTop: 10,
-    textAlign: 'center'
-    // padding: 10
-  },
-  divRight: {
-    height: '100%',
-    flex: 3,
-    margin: 10,
-    marginLeft: 60,
-    marginRight: 30,
-    textAlign: 'center'
-  },
-  paperLeft: {
-    height: 350,
-    marginBottom: 20,
-    textAlign: 'center'
-  },
-
-  title: {
-    marginBottom: 20
-  },
-  map: {
-    height: 400,
-    marginBottom: 30,
-    textAligh: 'center'
-  },
-  statistics: {
-    height: 300,
-    textAligh: 'center'
-  }
-};
 
 class TabContainer extends Component {
   componentDidMount() {
@@ -78,42 +28,15 @@ class TabContainer extends Component {
             <TestList info={all} />
           ))}
         </Typography>
-        <div>
-          <div style={styles.row}>
-            <div zDepth={3} style={styles.divLeft}>
-              <Paper style={styles.paperLeft}>
-                {/* ANCHOR Replace the Typography with weather component  */}
-                <Weather />
-              </Paper>
-              <Paper style={styles.paperLeft}>
-                {/* ANCHOR Replace the Typography with Overview UI component */}
-                <Typography>Overview</Typography>
-              </Paper>
-            </div>
-
-            <div zDepth={3} style={styles.divRight}>
-              <Typography variant='h5' align='left' style={styles.title}>
-                Dashboard
-              </Typography>
-              <Paper style={styles.map}>
-                {/* ANCHOR Replace the Typography with Map UI component */}
-                <MarkerPoint />
-              </Paper>
-              <Paper style={styles.statistics}>
-                {/* ANCHOR Replace the Typography with Statistics UI component */}
-                <Typography>Statistics UI</Typography>
-              </Paper>
-            </div>
-          </div>
-        </div>
+        {this.props.children}
       </React.Fragment>
     );
   }
 }
 
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired
-};
+// TabContainer.propTypes = {
+//   children: PropTypes.node.isRequired
+// };
 
 const mapStateToProps = state => ({
   alls: state.crisis.alls,
---
+++ frontend/src/components/layout/tabs/hazeTab.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import LocationSearchInput from '../../map/LocationSearchInput';
+import MarkerPoint from '../../map/Map';
+import TestList from '../testList';
+
+import { connect } from 'react-redux';
+import { getAll } from '../../../actions/crisisAction';
+
+const styles = {
+  row: {
+    display: 'flex',
+    flexDirection: 'row wrap',
+    width: '100%',
+    marginTop: 30
+  },
+  divLeft: {
+    flex: 1,
+    height: '100%',
+    margin: 30,
+    marginTop: 10,
+    textAlign: 'center'
+    // padding: 10
+  },
+  divRight: {
+    height: '100%',
+    flex: 3,
+    margin: 10,
+    marginLeft: 60,
+    marginRight: 30,
+    textAlign: 'center'
+  },
+  paperLeft: {
+    height: 350,
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+
+  title: {
+    marginBottom: 20
+  },
+  map: {
+    height: 400,
+    marginBottom: 30,
+    textAligh: 'center'
+  },
+  statistics: {
+    height: 300,
+    textAligh: 'center'
+  }
+};
+
+class HazeTab extends Component {
+  componentDidMount() {
+    this.props.getAll();
+  }
+
+  render() {
+    const { alls } = this.props;
+    return (
+      <React.Fragment>
+        <div>
+          <div style={styles.row}>
+            <div zDepth={3} style={styles.divLeft}>
+              <Paper style={styles.paperLeft}>
+                {/* ANCHOR Replace the Typography with weather component  */}
+              </Paper>
+              <Paper style={styles.paperLeft}>
+                {/* ANCHOR Replace the Typography with Overview UI component */}
+                <Typography>Overview</Typography>
+              </Paper>
+            </div>
+
+            <div zDepth={3} style={styles.divRight}>
+              <Typography variant='h5' align='left' style={styles.title}>
+                Dashboard
+              </Typography>
+              <Paper style={styles.map}>
+                {/* ANCHOR Replace the Typography with Map UI component */}
+                <MarkerPoint />
+              </Paper>
+              <Paper style={styles.statistics}>
+                {/* ANCHOR Replace the Typography with Statistics UI component */}
+                <Typography>Statistics UI</Typography>
+              </Paper>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+HazeTab.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+const mapStateToProps = state => ({
+  alls: state.crisis.alls,
+  dengues: state.crisis.dengues,
+  hazes: state.crisis.hazes
+});
+
+export default connect(
+  mapStateToProps,
+  { getAll }
+)(HazeTab);
