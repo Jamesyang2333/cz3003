c37b62cc33d9663570c0ea61e320ef5d2ec899ec
guomk <gmk19990312@gmail.com>
2019-03-27 14:27:38 +0800

    Finish Rest API

--- backend/ccs/serializers.py
+++ backend/ccs/serializers.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .model import Record
+from .models import Record
 
 
 class HazeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Record
+        fields = '__all__'
+
+
+class DengueSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Record
+        fields = '__all__'
+
+
+class AllSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Record
+        fields = '__all__'
