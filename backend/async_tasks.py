a769df1fe3055768bd8fffc523ab579b3d64d48f
James Yang <jamesyang2333@gmail.com>
2019-04-11 00:12:41 +0800

    fix bug

--- backend/async_tasks.py
+++ backend/async_tasks.py
@@ -7,12 +7,7 @@ sms_manager.delay()
 facebook_manager.delay()
 email_manager.delay()
 
-<<<<<<< HEAD
 # facebook_manager.delay() 
 #can call this as many times as you want cuz it will check if the database already has data. if not, add data to database
-=======
-# can call this as many times as you want cuz it will check if the database already has data. if not, add data to database
-
->>>>>>> c7d18ae5090d834cd9bcff162cee3dfef5826acc
 # print(Guideline.objects.all())
 # #postGuidelines('Dengue')
