4497bf6fe5a8b1eef9eb4abd95d286e514312143
Zhiwei Guo <goozwjenny@gmail.com>
2019-04-11 14:23:55 +0800

    modified admin.py

--- backend/ccs/admin.py
+++ backend/ccs/admin.py
@@ -5,10 +5,10 @@ from .models import Record
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('record_id', 'date', 'incident_type', 'number_of_injured', 'number_of_death', 'incident_status',
+    list_display = ('record_id', 'incident_location', 'incident_type', 'number_of_injured', 'number_of_death', 'incident_status',
                     'incident_region')
-    list_filter = ('date', 'incident_status', 'incident_type')
-    ordering = ['incident_status', 'date']
+    list_filter = ('date', 'incident_status', 'incident_type','incident_region')
+    ordering = ['record_id']
 
 
 admin.site.register(Record, PostAdmin)
